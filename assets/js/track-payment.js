e290f1205861e9fe06bbd49c7d622e2d6a0ac1d9
Chinonso Alexander <chinonsoalexander2@gmail.com>
2025-12-06 15:08:04 +0100

    second commit

--- assets/js/track-payment.js
+++ assets/js/track-payment.js
@@ -4,6 +4,6 @@ document.getElementById("trackForm").addEventListener("submit", function (e) {
     let ref = document.getElementById("refInput").value.trim();
 
     if (ref !== "") {
-        window.location.href = "https://willers-solution.vercel.app/paymentt?reference=" + encodeURIComponent(ref);
+        window.location.href = "https://nysc.willerssolutions.com/paymentt?reference=" + encodeURIComponent(ref);
     }
 });
